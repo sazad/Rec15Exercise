e28c921fe49eab62fc3dce3df5c91072208f332f
Sonia <sazad@andrew.cmu.edu>
2015-04-29 12:57:40 -0400

    made toast

--- src/main/java/edu/cmu/cs/cs214/rec15/client/ChatClientImpl.java
+++ src/main/java/edu/cmu/cs/cs214/rec15/client/ChatClientImpl.java
@@ -5,9 +5,9 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.List;
-import java.net.SocketException;
 
 import edu.cmu.cs.cs214.rec15.server.Message;
 import edu.cmu.cs.cs214.rec15.util.Log;
@@ -19,146 +19,137 @@ import edu.cmu.cs.cs214.rec15.util.Log;
  *
  */
 public class ChatClientImpl extends Thread implements ChatClient {
-    private static final String TAG = "CLIENT";
-    private Socket socket = null;
-    private String username;
-    private ObjectOutputStream out;
-    private List<ClientChangeListener> listeners = new ArrayList<ClientChangeListener>();
+	private static final String TAG = "CLIENT";
+	private Socket socket = null;
+	private String username;
+	private ObjectOutputStream out;
+	private List<ClientChangeListener> listeners = new ArrayList<ClientChangeListener>();
 
+	@Override
+	public void addClientChangeListener(ClientChangeListener listener) {
+		listeners.add(listener);
+	}
 
-    @Override
-    public void addClientChangeListener(ClientChangeListener listener) {
-        listeners.add(listener);
-    }
+	@Override
+	public void removeClientChangeListener(ClientChangeListener listener) {
+		listeners.remove(listener);
+	}
 
+	@Override
+	public boolean sendMessage(String message) {
+		try {
+			Message msg = new Message(message, username);
+			out.writeObject(msg);
+			return true;
+		} catch (SocketException e) {
+			try {
+				socket.close();
+			} catch (IOException e1) {
+				Log.e(TAG, "Unable to close socket.");
+			}
+			Log.e(TAG, "Server closed connection.");
+		} catch (IOException e) {
+			Log.e(TAG, "Unable to send message to server.");
+		}
+		return false;
+	}
 
-    @Override
-    public void removeClientChangeListener(ClientChangeListener listener) {
-        listeners.remove(listener);
-    }
+	@Override
+	public void setUsername(String name) {
+		this.username = name;
+	}
 
+	@Override
+	public String getUsername() {
+		return this.username;
+	}
 
-    @Override
-    public boolean sendMessage(String message) {
-        try {
-            Message msg = new Message(message, username);
-            out.writeObject(msg);
-            return true;
-        } catch (SocketException e) {
-            try {
-                socket.close();
-            } catch (IOException e1) {
-                Log.e(TAG, "Unable to close socket.");
-            }
-            Log.e(TAG, "Server closed connection.");
-        } catch (IOException e) {
-            Log.e(TAG, "Unable to send message to server.");
-        }
-        return false;
-    }
+	@Override
+	public void connectToServer(String host, int port) {
+		// Terminate any existing connections
+		if (socket != null) {
+			try {
+				socket.close();
+			} catch (IOException e) {
+				// Ignore the error
+				Log.e(TAG, "Unable to terminate connection with previous host");
+			}
+		}
+		// Connect to the new server
+		try {
+			Log.i(TAG, String.format("Connected to server %s:%d.", host, port));
+			socket = new Socket(host, port);
+			out = new ObjectOutputStream(socket.getOutputStream());
+			Log.i(TAG, "New person " + getUsername() + " joined");
+			// Tell server who is connecting
+			out.writeObject(getUsername());
+		} catch (IllegalArgumentException e) {
+			Log.e(TAG, e.toString());
+			return;
+		} catch (IOException e) {
+			Log.e(TAG, String.format("Could not connect to %s:%d", host, port));
+			return;
+		}
 
+		this.start();
+		notifyListenersConnectedToServer(host, port);
+	}
 
-    @Override
-    public void setUsername(String name) {
-        this.username = name;
-    }
+	@Override
+	public boolean isConnected() {
+		return (socket != null) && !socket.isClosed();
+	}
 
+	@Override
+	public void run() {
+		try {
+			while (true) {
+				ObjectInputStream in = new ObjectInputStream(
+						socket.getInputStream());
+				Message msg = (Message) in.readObject();
+				this.notifyListenersMessageSent(msg);
+				Log.i(TAG, String.format("Sending message: %s", msg));
+			}
+		} catch (EOFException e) {
+			Log.i(TAG, "Connected closed by server");
+			return;
+		} catch (IOException e) {
+			Log.e(TAG, e.toString());
+		} catch (ClassNotFoundException e) {
+			Log.e(TAG, e.toString());
+		} finally {
+			try {
+				socket.close();
+			} catch (IOException e) {
+				// Ignore, about to exit.
+				return;
+			}
+		}
+	}
 
-    @Override
-    public String getUsername() {
-        return this.username;
-    }
+	/**
+	 * notify all listeners of successful connection to server
+	 * 
+	 * @param host
+	 *            IP address of the ChatServer
+	 * @param port
+	 *            being used by the ChatServer
+	 */
+	private void notifyListenersConnectedToServer(String host, int port) {
+		for (ClientChangeListener listener : this.listeners) {
+			listener.startChat(getUsername(), Integer.toString(port), host);
+		}
+	}
 
-
-    @Override
-    public void connectToServer(String host, int port) {
-        // Terminate any existing connections
-        if (socket != null) {
-            try {
-                socket.close();
-            } catch (IOException e) {
-                // Ignore the error
-                Log.e(TAG, "Unable to terminate connection with previous host");
-            }
-        }
-        // Connect to the new server
-        try {
-            Log.i(TAG, String.format("Connected to server %s:%d.", host, port));
-            socket = new Socket(host, port);
-            out = new ObjectOutputStream(socket.getOutputStream());
-            // Tell server who is connecting
-            out.writeObject(getUsername());
-        } catch (IllegalArgumentException e) {
-            Log.e(TAG, e.toString());
-            return;
-        } catch (IOException e) {
-            Log.e(TAG, String.format("Could not connect to %s:%d", host, port));
-            return;
-        }
-
-        this.start();
-        notifyListenersConnectedToServer(host, port);
-    }
-
-
-    @Override
-    public boolean isConnected() {
-        return (socket != null) && !socket.isClosed();
-    }
-
-
-    @Override
-    public void run() {
-        try {
-            while (true) {
-                ObjectInputStream in = new ObjectInputStream(
-                        socket.getInputStream());
-                Message msg = (Message) in.readObject();
-                this.notifyListenersMessageSent(msg);
-                Log.i(TAG, String.format("Sending message: %s", msg));
-            }
-        } catch (EOFException e) {
-            Log.i(TAG, "Connected closed by server");
-            return;
-        } catch (IOException e) {
-            Log.e(TAG, e.toString());
-        } catch (ClassNotFoundException e) {
-            Log.e(TAG, e.toString());
-        } finally {
-            try {
-                socket.close();
-            } catch (IOException e) {
-                // Ignore, about to exit.
-                return;
-            }
-        }
-    }
-
-
-    /**
-     * notify all listeners of successful connection to server
-     * 
-     * @param host
-     *            IP address of the ChatServer
-     * @param port
-     *            being used by the ChatServer
-     */
-    private void notifyListenersConnectedToServer(String host, int port) {
-        for (ClientChangeListener listener : this.listeners) {
-            listener.startChat(getUsername(), Integer.toString(port), host);
-        }
-    }
-
-
-    /**
-     * notify all listeners that a message was sent
-     * 
-     * @param message
-     *            text of message being sent
-     */
-    private void notifyListenersMessageSent(Message message) {
-        for (ClientChangeListener listener : this.listeners) {
-            listener.messageReceived(message);
-        }
-    }
+	/**
+	 * notify all listeners that a message was sent
+	 * 
+	 * @param message
+	 *            text of message being sent
+	 */
+	private void notifyListenersMessageSent(Message message) {
+		for (ClientChangeListener listener : this.listeners) {
+			listener.messageReceived(message);
+		}
+	}
 }
